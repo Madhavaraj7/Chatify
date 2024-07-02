75a1eaea74c775f0939130dcdad5a5b588bf3491
Madhavaraj R <raj266864@gmail.com>
2024-07-02 12:51:52 +0530

    fetchChats

--- backend/controller/chatControllers.js
+++ backend/controller/chatControllers.js
@@ -49,3 +49,23 @@ export const accessChat = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
+
+export const fetchChats = async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { protect } from "../middleware/authMiddleware.js";
-import { accessChat } from "../controller/chatControllers.js";
+import { accessChat, fetchChats } from "../controller/chatControllers.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
+router.route("/").get(protect, fetchChats);
+
 
 
 export default router;
