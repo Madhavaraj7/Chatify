fd42356a0b557504bb002a056c82bbea1f2a1810
Madhavaraj R <raj266864@gmail.com>
2024-07-22 19:08:10 +0530

    crt

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,9 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   console.log('Production mode');
   app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  console.log('Serving static files from:', path.join(__dirname, "frontend", "dist"))
+
+
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
@@ -70,7 +73,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
 // Set up Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"], // Add any other origins as needed
+    origin: ["http://localhost:5000", "http://localhost:5173"], // Add any other origins as needed
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
