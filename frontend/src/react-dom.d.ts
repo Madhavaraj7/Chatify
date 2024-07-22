6ea983c4e7015afc05e9e321f0d529c5463d1a07
Madhavaraj R <raj266864@gmail.com>
2024-07-22 19:28:48 +0530

    okay

---
+++ frontend/src/react-dom.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-dom' {
+    import * as ReactDOM from 'node_modules/react-dom/index';
+    export = ReactDOM;
+  }
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
-/// <reference types="vite/client" />
+// vite-env.d.ts
+declare module 'vite' {
+    const content: any;
+    export default content;
+  }
+  
+  declare module '@vitejs/plugin-react' {
+    const content: any;
+    export default content;
+  }
+  
